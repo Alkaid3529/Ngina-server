c3d4452fbeb297218c98d7f3fe95d4bd0e3b5c46
Alkaid <98629981+Alkaid3529@users.noreply.github.com>
2023-01-20 14:35:46 +0800

    Update ngx_global.h

--- _include/ngx_global.h
+++ _include/ngx_global.h
@@ -1,4 +1,4 @@
-﻿
+
 #ifndef __NGX_GBLDEF_H__
 #define __NGX_GBLDEF_H__
 
@@ -32,12 +32,20 @@ typedef struct
 } ngx_log_t;
 
 // 外部全局变量声明
+
+// 保存 argv 参数所需内存空间大小
 extern size_t g_argvneedmem;
+// 环境变量所占内存大小
 extern size_t g_envneedmem;
+// 参数个数
 extern int g_os_argc;
+// 原始命令行参数数组
 extern char **g_os_argv;
+// 指向自己分配的环境变量保存位置
 extern char *gp_envmem;
+// 守护进程标记
 extern int g_daemonized;
+
 extern CLogicSocket g_socket;
 extern CThreadPool g_threadpool;
 
